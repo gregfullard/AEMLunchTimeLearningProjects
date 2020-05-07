2ef0bd760720f536fb5714e657582fad2edbb172
Greg Fullard <gfullard@gmail.com>
2020-05-07 08:05:33 +0200

    Adding day 10 code - Simple Servlet

---
+++ LTL_D10/demo/core/src/main/java/org/redquark/demo/core/services/ReadJsonService.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package org.redquark.demo.core.services;
+
+/**
+ * @author gregf
+ *
+ */
+public interface ReadJsonService {
+
+	/**
+	 * @return JSON String
+	 */
+	public String getData();
+}
